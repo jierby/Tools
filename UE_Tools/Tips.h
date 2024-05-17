c6c284dfa3950821ba7b9652d1042cd3deb13a40
jierby <54579150+jierby@users.noreply.github.com>
2024-05-17 19:07:51 +0800

    Update Tips.h

--- UE_Tools/Tips.h
+++ UE_Tools/Tips.h
@@ -6,3 +6,9 @@
       //bool bSuccess = DesktopPlatform->OpenFileDialog(nullptr,TEXT("打开文件"))
       sussess = FFileHelper::SaveStringToFile(_Text, *_filePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
   }
+//读取文件为String,可以使用蓝图节点ParseIntoArray进行分割
+UFUNCTION(BlueprintCallable, Category = "GWorld|Flib|AssetManagerEx")
+	static bool LoadFileToString(const FString& InFile, FString& OutString)
+  {
+      return FFileHelper::LoadFileToString(OutString, *InFile);
+  }
